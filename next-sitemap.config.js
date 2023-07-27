97ca9ae6965b78487f7ade6beeee4040173e489d
xion <calf-love@naver.com>
2023-07-27 15:09:10 +0900

    :sparkles: Vscode setting
    add

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://devxion-blog.vercel.app/',
+  siteUrl: 'https://localhost:3000',
   changefreq: 'daily',
   priority: 0.7,
   sitemapSize: 7000,
